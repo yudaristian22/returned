527c2c6bb156793aa50a7999cd0f174acf8740be
Yuda Ristian Asgari <yudaramasenju@gmail.com>
2023-11-28 18:20:35 +0700

    update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Nav Bar
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
@@ -12,4 +13,21 @@ if (close) {
     close.addEventListener('click', () => {
         nav.classList.remove('active')
     })
+}
+
+// Single Product
+var MainImg = document.getElementById("MainImg");
+var smallimg = document.getElementsByClassName("small-img");
+
+smallimg[0].onclick = function(){
+    MainImg.src = smallimg[0].src;
+}
+smallimg[1].onclick = function(){
+    MainImg.src = smallimg[1].src;
+}
+smallimg[2].onclick = function(){
+    MainImg.src = smallimg[2].src;
+}
+smallimg[3].onclick = function(){
+    MainImg.src = smallimg[3].src;
 }
